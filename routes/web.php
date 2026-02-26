08430dee0d0cb30eea0a7699c835aaf4a8e50422
Lorenzo05-creator <lory4jz@gmail.com>
2026-02-26 11:56:29 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,16 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PublicController;
 
-Route::get('/', [PublicController::class, 'index'])->name('home');
-Route::resource('posts', PublicController::class);
+Route::get('/', function () {
+    return view('home');
+})->name('home');
+
+Route::get('/posts', [PublicController::class, 'index'])->name('posts.index');
+Route::get('/posts/create', [PublicController::class, 'create'])->name('posts.create');
+Route::post('/posts', [PublicController::class, 'store'])->name('posts.store');
+Route::get('/posts/{post}', [PublicController::class, 'show'])->name('posts.show');
+Route::get('/posts/{post}/edit', [PublicController::class, 'edit'])->name('posts.edit');
+Route::put('/posts/{post}', [PublicController::class, 'update'])->name('posts.update');
+Route::delete('/posts/{post}', [PublicController::class, 'destroy'])->name('posts.destroy');
