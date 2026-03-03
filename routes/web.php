13a5c752b9456619a34a82412eaea31619337589
Lorenzo05-creator <lory4jz@gmail.com>
2026-03-03 11:54:51 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -7,10 +7,12 @@ Route::get('/', function () {
     return redirect()->route('posts.index');
 })->name('home');
 
-Route::get('/posts', [PublicController::class, 'index'])->name('posts.index');
-Route::get('/posts/create', [PublicController::class, 'create'])->name('posts.create');
-Route::post('/posts', [PublicController::class, 'store'])->name('posts.store');
-Route::get('/posts/{post}', [PublicController::class, 'show'])->name('posts.show');
-Route::get('/posts/{post}/edit', [PublicController::class, 'edit'])->name('posts.edit');
-Route::put('/posts/{post}', [PublicController::class, 'update'])->name('posts.update');
-Route::delete('/posts/{post}', [PublicController::class, 'destroy'])->name('posts.destroy');
+Route::middleware('auth')->group(function () {
+    Route::get('/posts', [PublicController::class, 'index'])->name('posts.index');
+    Route::get('/posts/create', [PublicController::class, 'create'])->name('posts.create');
+    Route::post('/posts', [PublicController::class, 'store'])->name('posts.store');
+    Route::get('/posts/{post}', [PublicController::class, 'show'])->name('posts.show');
+    Route::get('/posts/{post}/edit', [PublicController::class, 'edit'])->name('posts.edit');
+    Route::put('/posts/{post}', [PublicController::class, 'update'])->name('posts.update');
+    Route::delete('/posts/{post}', [PublicController::class, 'destroy'])->name('posts.destroy');
+});
