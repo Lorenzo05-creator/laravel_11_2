96bb91b499cb17c4b3b2bce282cd66c99e68a69d
Lorenzo05-creator <lory4jz@gmail.com>
2026-03-24 14:39:08 +0100

    Update edit.blade.php

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -1,27 +1,18 @@
 @extends('layouts.app')
 
-@section('title', 'Modifica articolo')
-
 @section('content')
-    <h1 class="mb-4">Modifica articolo</h1>
+    <h1>Modifica Post</h1>
 
     <form action="{{ route('posts.update', $post) }}" method="POST">
         @csrf
         @method('PUT')
 
-        <div class="mb-3">
-            <label class="form-label">Titolo</label>
-            <input type="text" name="title" class="form-control" value="{{ old('title', $post->title) }}">
-            @error('title') <div class="text-danger small">{{ $message }}</div> @enderror
-        </div>
+        <label>Titolo</label>
+        <input type="text" name="title" value="{{ $post->title }}">
 
-        <div class="mb-3">
-            <label class="form-label">Contenuto</label>
-            <textarea name="content" rows="6" class="form-control">{{ old('content', $post->content) }}</textarea>
-            @error('content') <div class="text-danger small">{{ $message }}</div> @enderror
-        </div>
+        <label>Contenuto</label>
+        <textarea name="body">{{ $post->body }}</textarea>
 
-        <button type="submit" class="btn btn-primary">Aggiorna</button>
-        <a href="{{ route('posts.show', $post) }}" class="btn btn-secondary">Annulla</a>
+        <button type="submit">Aggiorna</button>
     </form>
 @endsection
