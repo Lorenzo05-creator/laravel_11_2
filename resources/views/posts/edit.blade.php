04102c006492609fbb26f73b1c86da533562e8b0
Lorenzo05-creator <lory4jz@gmail.com>
2026-03-24 14:41:17 +0100

    Update edit.blade.php

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -1,18 +1,22 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>Modifica Post</h1>
+<h1 class="mb-4">Modifica Post</h1>
 
-    <form action="{{ route('posts.update', $post) }}" method="POST">
-        @csrf
-        @method('PUT')
+<form action="{{ route('posts.update', $post) }}" method="POST">
+    @csrf
+    @method('PUT')
 
-        <label>Titolo</label>
-        <input type="text" name="title" value="{{ $post->title }}">
+    <div class="mb-3">
+        <label class="form-label">Titolo</label>
+        <input type="text" name="title" class="form-control" value="{{ $post->title }}">
+    </div>
 
-        <label>Contenuto</label>
-        <textarea name="body">{{ $post->body }}</textarea>
+    <div class="mb-3">
+        <label class="form-label">Contenuto</label>
+        <textarea name="body" class="form-control" rows="5">{{ $post->body }}</textarea>
+    </div>
 
-        <button type="submit">Aggiorna</button>
-    </form>
+    <button class="btn btn-warning">Aggiorna</button>
+</form>
 @endsection
