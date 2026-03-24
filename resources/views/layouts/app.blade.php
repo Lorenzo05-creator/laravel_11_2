90fc94d3e0472850c0cdc5582f75070ca8d79fc4
Lorenzo05-creator <lory4jz@gmail.com>
2026-03-24 14:37:27 +0100

    app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,19 +2,36 @@
 <html lang="it">
 <head>
     <meta charset="UTF-8">
-    <title>@yield('title', 'Blog')</title>
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <title>Blog Laravel</title>
 </head>
 <body>
-<nav class="navbar navbar-expand-lg bg-body-tertiary mb-4">
-    <div class="container">
-        <a class="navbar-brand" href="{{ route('posts.index') }}">Blog</a>
-        <a class="btn btn-primary" href="{{ route('posts.create') }}">Nuovo articolo</a>
-    </div>
-</nav>
 
-<div class="container">
+    <nav>
+        <ul>
+            <li><a href="{{ route('home') }}">Home</a></li>
+            <li><a href="{{ route('posts.index') }}">Posts</a></li>
+
+            @auth
+                <li><a href="{{ route('posts.create') }}">Crea Post</a></li>
+
+                <li>
+                    <form action="/logout" method="POST">
+                        @csrf
+                        <button type="submit">Logout</button>
+                    </form>
+                </li>
+            @endauth
+
+            @guest
+                <li><a href="/login">Login</a></li>
+                <li><a href="/register">Registrati</a></li>
+            @endguest
+        </ul>
+    </nav>
+
+    <hr>
+
     @yield('content')
-</div>
+
 </body>
 </html>
