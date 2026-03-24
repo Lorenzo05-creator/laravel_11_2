fc38ce6a7f003168dbb08eb8d0fed0011944a981
Lorenzo05-creator <lory4jz@gmail.com>
2026-03-24 14:40:50 +0100

    Update create.blade.php

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -1,17 +1,21 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>Crea Post</h1>
+<h1 class="mb-4">Crea Post</h1>
 
-    <form action="{{ route('posts.store') }}" method="POST">
-        @csrf
+<form action="{{ route('posts.store') }}" method="POST">
+    @csrf
 
-        <label>Titolo</label>
-        <input type="text" name="title">
+    <div class="mb-3">
+        <label class="form-label">Titolo</label>
+        <input type="text" name="title" class="form-control">
+    </div>
 
-        <label>Contenuto</label>
-        <textarea name="body"></textarea>
+    <div class="mb-3">
+        <label class="form-label">Contenuto</label>
+        <textarea name="body" class="form-control" rows="5"></textarea>
+    </div>
 
-        <button type="submit">Salva</button>
-    </form>
+    <button class="btn btn-success">Salva</button>
+</form>
 @endsection
