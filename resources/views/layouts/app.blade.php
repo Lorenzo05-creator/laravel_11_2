d0bf0222f25e27aff19b014562e86d5c1a12d749
Lorenzo05-creator <lory4jz@gmail.com>
2026-03-24 14:40:05 +0100

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,35 +3,53 @@
 <head>
     <meta charset="UTF-8">
     <title>Blog Laravel</title>
+
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
 
-    <nav>
-        <ul>
-            <li><a href="{{ route('home') }}">Home</a></li>
-            <li><a href="{{ route('posts.index') }}">Posts</a></li>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container">
+        <a class="navbar-brand" href="{{ route('home') }}">Blog</a>
 
-            @auth
-                <li><a href="{{ route('posts.create') }}">Crea Post</a></li>
+        <div>
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
 
-                <li>
-                    <form action="/logout" method="POST">
-                        @csrf
-                        <button type="submit">Logout</button>
-                    </form>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('posts.index') }}">Posts</a>
                 </li>
-            @endauth
 
-            @guest
-                <li><a href="/login">Login</a></li>
-                <li><a href="/register">Registrati</a></li>
-            @endguest
-        </ul>
-    </nav>
+                @auth
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('posts.create') }}">Crea Post</a>
+                    </li>
 
-    <hr>
+                    <li class="nav-item">
+                        <form action="/logout" method="POST">
+                            @csrf
+                            <button class="btn btn-danger btn-sm ms-3">Logout</button>
+                        </form>
+                    </li>
+                @endauth
 
+                @guest
+                    <li class="nav-item">
+                        <a class="nav-link" href="/login">Login</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/register">Registrati</a>
+                    </li>
+                @endguest
+
+            </ul>
+        </div>
+    </div>
+</nav>
+
+<div class="container mt-4">
     @yield('content')
+</div>
 
 </body>
 </html>
