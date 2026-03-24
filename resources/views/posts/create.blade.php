57cc1546b0de68ed64f850b840ffb5e44c3e989b
Lorenzo05-creator <lory4jz@gmail.com>
2026-03-24 14:38:29 +0100

    create.blade.php

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -1,26 +1,17 @@
 @extends('layouts.app')
 
-@section('title', 'Nuovo articolo')
-
 @section('content')
-    <h1 class="mb-4">Crea un nuovo articolo</h1>
+    <h1>Crea Post</h1>
 
     <form action="{{ route('posts.store') }}" method="POST">
         @csrf
 
-        <div class="mb-3">
-            <label class="form-label">Titolo</label>
-            <input type="text" name="title" class="form-control" value="{{ old('title') }}">
-            @error('title') <div class="text-danger small">{{ $message }}</div> @enderror
-        </div>
+        <label>Titolo</label>
+        <input type="text" name="title">
 
-        <div class="mb-3">
-            <label class="form-label">Contenuto</label>
-            <textarea name="content" rows="6" class="form-control">{{ old('content') }}</textarea>
-            @error('content') <div class="text-danger small">{{ $message }}</div> @enderror
-        </div>
+        <label>Contenuto</label>
+        <textarea name="body"></textarea>
 
-        <button type="submit" class="btn btn-primary">Salva</button>
-        <a href="{{ route('posts.index') }}" class="btn btn-secondary">Annulla</a>
+        <button type="submit">Salva</button>
     </form>
 @endsection
