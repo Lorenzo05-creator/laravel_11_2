dbcfd90b70366a7fd31b4a3d155b2fc4f482ed7d
Lorenzo05-creator <lory4jz@gmail.com>
2026-03-24 14:40:25 +0100

    Update index.blade.php

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,25 +1,38 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>Lista Post</h1>
+<h1 class="mb-4">Lista Post</h1>
 
+<div class="row">
     @foreach ($posts as $post)
-        <div>
-            <h2>{{ $post->title }}</h2>
-            <p>{{ $post->body }}</p>
+        <div class="col-md-4">
+            <div class="card mb-4 shadow-sm">
+                <div class="card-body">
+                    <h5 class="card-title">{{ $post->title }}</h5>
+                    <p class="card-text">
+                        {{ Str::limit($post->body, 100) }}
+                    </p>
 
-            <a href="{{ route('posts.show', $post) }}">Vedi</a>
+                    <a href="{{ route('posts.show', $post) }}" class="btn btn-primary btn-sm">
+                        Leggi
+                    </a>
 
-            @auth
-                <a href="{{ route('posts.edit', $post) }}">Modifica</a>
+                    @auth
+                        <a href="{{ route('posts.edit', $post) }}" class="btn btn-warning btn-sm">
+                            Modifica
+                        </a>
 
-                <form action="{{ route('posts.destroy', $post) }}" method="POST">
-                    @csrf
-                    @method('DELETE')
-                    <button type="submit">Elimina</button>
-                </form>
-            @endauth
+                        <form action="{{ route('posts.destroy', $post) }}" method="POST" class="d-inline">
+                            @csrf
+                            @method('DELETE')
+                            <button class="btn btn-danger btn-sm">
+                                Elimina
+                            </button>
+                        </form>
+                    @endauth
+                </div>
+            </div>
         </div>
-        <hr>
     @endforeach
+</div>
 @endsection
